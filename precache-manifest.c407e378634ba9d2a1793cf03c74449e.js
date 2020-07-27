81a054841d14f2771b95e35cce30a11fcf2ebbda
Gonzalo Galvez <ggalvezb@live.com>
2020-07-27 17:19:34 -0400

    Updates

--- precache-manifest.c407e378634ba9d2a1793cf03c74449e.js
+++ precache-manifest.c407e378634ba9d2a1793cf03c74449e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9c279d8157d4134ff699a79228aebda1",
+    "revision": "06762fdfb684ea1dd8955c6f4f1b29f5",
     "url": "/cv/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cv/static/css/2.e4346227.chunk.css"
   },
   {
-    "revision": "1281f3e6046deb7daed8",
+    "revision": "a23f66e3e2a306184baa",
     "url": "/cv/static/css/main.7a66541f.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cv/static/js/2.0f394e2f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1281f3e6046deb7daed8",
-    "url": "/cv/static/js/main.a6248dfe.chunk.js"
+    "revision": "a23f66e3e2a306184baa",
+    "url": "/cv/static/js/main.f7a0d747.chunk.js"
   },
   {
     "revision": "9e0e921495960402e4a4",
